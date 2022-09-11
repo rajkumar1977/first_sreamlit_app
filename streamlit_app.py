a1f0e6a8b599ab240abca08a656b6d928053ef6d
Rajkumar <105517384+rajkumar1977@users.noreply.github.com>
2022-09-11 19:30:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,6 @@ streamlit.dataframe(my_fruit_list)
 import pandas
 my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #let put picklist
-streamlit.multiselect("Pick Some fruits:",list(my_fruit_list.Index))
+streamlit.multiselect("Pick Some fruits:",list(my_fruit_list.index))
 #display table on range
 streamlit.dataframe(my_fruit_list)
