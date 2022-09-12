9e6435062fb9dfbd33ca52e478add28e6692c169
Rajkumar <105517384+rajkumar1977@users.noreply.github.com>
2022-09-12 18:25:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,11 +17,6 @@ streamlit.dataframe(fruits_to_show)
 
 
 
-import streamlit
-streamlit.title('Fruityvice Fruit Advice!')
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
 
 
 
