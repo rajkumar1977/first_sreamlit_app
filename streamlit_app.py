ce9f786dd98f9814f5eac6f1c70d38248caa4d6e
Rajkumar <105517384+rajkumar1977@users.noreply.github.com>
2022-09-12 18:14:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ streamlit.dataframe(my_fruit_list)
 import pandas
 my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
-
+streamlit.dataframe(my_fruit_list)
 
 import streamlit
 streamlit.title('Fruityvice Fruit Advice!')
