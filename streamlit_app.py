466270645a8b98a928d8bf5aeca193b4bf6d7049
Rajkumar <105517384+rajkumar1977@users.noreply.github.com>
2022-09-13 03:22:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,13 +34,12 @@ if not fruit_choice:
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_noramalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
-
 except URLError as e:
   streamlit.error()
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 # don't run anything past here while we troublshoot
 streamlit.stop()
 #import snowflake.connector
